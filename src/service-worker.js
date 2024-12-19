7dccc1830df8a4589abbcb296a8e99aeb2375654
Nam Vu <vnt87@yahoo.com>
2024-12-19 20:03:57 +0700

    update PWA manifest
    and changed favicon

---
+++ src/service-worker.js
@@ -0,0 +1,22 @@
+
+const CACHE_NAME = 'textpod-v1';
+const urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/main.js',
+  // Add other assets you want to cache
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
